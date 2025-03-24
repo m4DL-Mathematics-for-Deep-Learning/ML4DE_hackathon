9b0cd7d63c3b2c0ad75ec1a57bcee9e54ce02a36
GeorgAUT <gam37@cam.ac.uk>
2025-03-24 18:04:55 +0000

    now try again

--- scoring/app.py
+++ scoring/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 import os
 
@@ -12,3 +12,6 @@ TEAMNAME_FILE = os.path.join(TEAM_FOLDER, 'teamname.txt')
 @app.route('/')
 def hello_world():
     return 'Hello, World!123213'
+
+def index():
+    return render_template("index.html", E1=1.0, E2=1.0)
